3f73fadaeb16632575e589f445a2bef69933a64f
lilyoo226 <samuellove228@gmail.com>
2022-11-08 19:14:15 +0000

    few

--- src/components/Project/ProjectContainer.jsx
+++ src/components/Project/ProjectContainer.jsx
@@ -4,7 +4,7 @@ const ProjectContainer = () => {
   return (
     <div class="project-container">
             <div class="project-left">
-              
+              <img src="https://images.unsplash.com/photo-1661956602153-23384936a1d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"  alt="" />
             </div>
             <div class="project-right">
               <div class="project-right-content">
