f7f95a3d63ff84868c076717e3ec7d8b24d31ae1
Samuel Amoah <94200301+lilyoo226@users.noreply.github.com>
2022-11-15 19:17:33 +0000

    ty

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import React from 'react'
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Main from "./layout/Main";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Archive from "./layout/Archive";
-import Footer from "./components/Footer/Footer";
-import Preloader from './components/Preloader/Preloader';
-const Homie = React.lazy(()=>import("./layout/Main"))
+import RubberBand from "./components/RuberBand/RubberBand";
+
+
 
 function App() {
   return (
-    <React.Suspense fallback={<Preloader/>}>
-    <Router>
-      
+    <BrowserRouter>
       <Navbar />
-      <Routes> 
-        <Route path="/" element={<Homie />} />
+ 
+      <Routes>
+        <Route path="/" element={<Main />} />
         <Route path="/archive" element={<Archive />} />
-        <Route path="*" element={<Main />}/>
       </Routes>
-      <Footer />
-    </Router>
-   </React.Suspense>
+
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-
