263c7f9b004b06a5cd5fb47d9b2eeb8efe288d17
Samuel Amoah <94200301+lilyoo226@users.noreply.github.com>
2022-11-15 22:13:58 +0000

    h

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 import "./Navbar.css";
+import Resume from "../../assets/pdf/cv.pdf";
 import { useScroll } from "./UseScroll";
 const Navbar = () => {
-  const [btnState, setbtnState] = useState(false)
+  const [btnState, setbtnState] = useState(false);
   const { scrollDirection } = useScroll();
-
 
   const styles = {
     active: {
@@ -25,22 +25,18 @@ const Navbar = () => {
     }
   };
 
-
-
-  function handleMenuIconClick(event){
-  
-    setbtnState(btnState => !btnState);
+  function handleMenuIconClick(event) {
+    setbtnState((btnState) => !btnState);
   }
 
-
-  let toggleClassCheck = btnState ? "change" : ""
+  let toggleClassCheck = btnState ? "change" : "";
 
   return (
     <nav style={homieScroll()}>
       <div className="container">
         <div className="logo">
           <Link to="/">
-          <img className="logo-logo" width="50px" src={logo} alt="" />
+            <img className="logo-logo" width="50px" src={logo} alt="" />
           </Link>
         </div>
         <div className={btnState ? "nav-links-mobile" : "links"}>
@@ -66,18 +62,21 @@ const Navbar = () => {
               </Link>
             </li>
             <li>
-              <Link id="button" className="button" href="#">
+              <Link id="button" className="button" download to={Resume}>
                 Resume
               </Link>
             </li>
-            <div id="menu-btn" onClick={()=>handleMenuIconClick()}  className={`menu-icon ${toggleClassCheck}`}>
-               <div class="menu-icon-container" onclick="myFunction(this)">
-              <div class="bar1"></div>
-              <div class="bar2"></div>
-              <div class="bar3"></div>
+            <div
+              id="menu-btn"
+              onClick={() => handleMenuIconClick()}
+              className={`menu-icon ${toggleClassCheck}`}
+            >
+              <div class="menu-icon-container" onclick="myFunction(this)">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+              </div>
             </div>
-            </div>
-           
           </ul>
         </div>
       </div>
